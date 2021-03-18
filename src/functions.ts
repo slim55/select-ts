76e41a37ecd8ed33d207c6e1f033123e11c1c99a
Вячеслав Иванов <slava.ivanov.omsk94@gmail.com>
2021-03-18 15:36:50 +0600

    feat: add function check mobile device

--- src/functions.ts
+++ src/functions.ts
@@ -6,4 +6,10 @@ export const toggleArrayItem: (
     ? arr.filter((elem: string) => elem !== item)
     : [...arr, item];
 
-export default { toggleArrayItem };
+export const checkMobileDevice: () => boolean = () => {
+  return !!/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/.exec(
+    window.navigator.userAgent
+  );
+};
+
+export default { toggleArrayItem, checkMobileDevice };
